d69f204db5d1c4a9e50f5867c85a1879ce42cdc9
XT <1804352454@qq.com>
2017-10-17 20:15:53 +0800

    删除错误文件及代码，使编译通过

--- MyApplication1/app/src/main/java/com/example/administrator/myapplication1/Part13.java
+++ MyApplication1/app/src/main/java/com/example/administrator/myapplication1/Part13.java
@@ -40,13 +40,13 @@ public class Part13 extends Activity {
                 startActivity(intent);
             }
         });
-        Button btn13_18 = (Button)findViewById(R.id.btn13_18);
-        btn13_18.setOnClickListener(new View.OnClickListener() {//html调webview
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Part13.this,HtmlToWebView.class);
-                startActivity(intent);
-            }
-        });
+//        Button btn13_18 = (Button)findViewById(R.id.btn13_18);
+//        btn13_18.setOnClickListener(new View.OnClickListener() {//html调webview
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Part13.this,HtmlToWebView.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
